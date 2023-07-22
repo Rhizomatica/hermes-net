40a36bac965c49b652a082fccf415846fef82fad
Rafael Diniz <rafael@riseup.net>
2023-07-22 02:04:31 +0100

    more code to i2ctest

--- utils/i2c_test.c
+++ utils/i2c_test.c
@@ -13,24 +13,34 @@ int main(int argc, char *argv[])
 {
     int bus = open("/dev/i2c-2", O_RDWR);
 
-    int addr = 0x08; /* The I2C address of the ATTiny85 */
-    if (ioctl(bus,I2C_SLAVE,addr) < 0)
+    int addr_at = 0x08; /* The I2C address of the ATTiny85 */
+    int addr_rtc = 0x68; /* The I2C address of the RTC */
+
+    if (ioctl(bus,I2C_SLAVE,addr_at) < 0)
+    {
+        /* ERROR HANDLING; you can check errno to see what went wrong */
+        exit(1);
+    }
+    uint8_t response[32];
+
+    int32_t count;
+
+    i2c_smbus_write_quick(bus, 0);
+    count = i2c_smbus_read_word_data(bus, 0);
+//count = i2c_smbus_read_i2c_block_data(bus, 0, 4, response);
+    printf("count = %d\n", count);
+
+    if (ioctl(bus,I2C_SLAVE,addr_rtc) < 0)
     {
         /* ERROR HANDLING; you can check errno to see what went wrong */
         exit(1);
     }
 
-    uint8_t response[32];
-
-    int32_t count;
-
-    // count = i2c_smbus_read_word_data(bus, 0);
-    // printf("count = %d\n");
     uint8_t values = 0;
     count = i2c_smbus_write_block_data(bus, 0, 1, &values);
-    count = i2c_smbus_read_i2c_block_data(bus, 0, 4, response);
+    count = i2c_smbus_read_i2c_block_data(bus, 0, 8, response);
 
-    printf("count = %d, read = 0x%x 0x%x 0x%x 0x%x\n", count, response[0], response[1], response[2], response[3]);
+    printf("count = %d, read = 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x 0x%x\n", count, response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7]);
 
     close(bus);
 
