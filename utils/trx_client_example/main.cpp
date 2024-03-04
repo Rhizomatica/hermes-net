794ee308342ea7d0cac361cb650d54cdb3a30809
Rafael Diniz <rafael@riseup.net>
2024-03-04 11:39:38 +0000

    added a sBitx shm interface sample

---
+++ utils/trx_client_example/main.cpp
@@ -0,0 +1,60 @@
+// C++ example for interacting with the sBitx radio daemon
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "radio_cmds.h"
+#include "sbitx_io.h"
+#include "shm_utils.h"
+
+int main(int argc, char *argv[])
+{
+    // ================== INITIALIZATION ==================
+    // This should be called just one, in the beginnig of any software
+    controller_conn *connector = NULL;
+
+    if (shm_is_created(SYSV_SHM_CONTROLLER_KEY_STR, sizeof(controller_conn)) == false)
+    {
+        fprintf(stderr, "Connector SHM not created. Is sbitx_controller running?\n");
+        return EXIT_FAILURE;
+    }
+
+    connector = (controller_conn *) shm_attach(SYSV_SHM_CONTROLLER_KEY_STR, sizeof(controller_conn));
+    // ================== INITIALIZATION ENDS =============
+
+
+
+    // Variables used to place the radio command and for its response
+    uint8_t srv_cmd[5];
+    uint8_t response[5];
+    bool cmd_resp;
+
+    memset(srv_cmd, 0, 5);
+    memset(response, 0, 5);
+
+    // ================== KEY ON ==================
+    srv_cmd[4] = CMD_PTT_ON;
+
+    cmd_resp = radio_cmd(connector, srv_cmd, response);
+
+    if (cmd_resp == false)
+        fprintf(stderr, "ERROR\n");
+
+    // ================== KEY ON ENDS =============
+
+    sleep(5);
+
+    // ================== KEY OFF =================
+    srv_cmd[4] = CMD_PTT_OFF;
+
+    cmd_resp = radio_cmd(connector, srv_cmd, response);
+
+    if (cmd_resp == false)
+        fprintf(stderr, "ERROR\n");
+
+    // ================== KEY OFF ENDS ============
+
+    return EXIT_SUCCESS;
+}
