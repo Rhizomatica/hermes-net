7873b8e8a9f9938a9b76e5d6b324bd43d6a32b8d
Rafael Diniz <rafael@riseup.net>
2024-06-16 01:00:09 +0100

    adding more commented code

--- trx_v2-firmware/attiny85.ino
+++ trx_v2-firmware/attiny85.ino
@@ -2,16 +2,23 @@
 // Authors: Ashhar Farhan and Rafael Diniz
 
 #include <Wire.h>
+// #include <EEPROM.h>
 
 int16_t fwd, ref;
 byte message[4];
+
+/*
+** PB2 (SCL) and PB0 (SDA) are I2C
+** PB1 is unused
+** PB3 (vfwd) and PB4 (vref)
+*/
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent()
 {
-    fwd = analogRead(A2);
-    ref = analogRead(A3);
+    fwd = analogRead(PB3);
+    ref = analogRead(PB4);
     message[0] = fwd & 0xff;
     message[1] = fwd >> 8;
     message[2] = ref & 0xff;
@@ -23,8 +30,26 @@ void setup()
 {
     Wire.begin(8);                // join i2c bus with address #8
     Wire.onRequest(requestEvent); // register event
+
+    // we could use PB1 for something
+   //  pinMode(PB1, OUTPUT);
+    pinMode(PB3, INPUT);
+    pinMode(PB4, INPUT);
+
+
 }
+
+// bool blink = 0;
+// uint16_t serial_number = 247;
+// uint8_t low = serial_number & 0xff;
+// uint8_t high = serial_number >> 8;
+// EEPROM.write(0, low);
+// EEPROM.write(1, high);
 
 void loop()
 {
+//    digitalWrite(PB1, HIGH);
+//    delay(5000);
+//    digitalWrite(PB1, LOW);
+//    delay(5000);
 }
