5bda6a240703f69492566c34b0af94b166bd9804
Rafael Diniz <rafael@riseup.net>
2024-06-24 22:00:44 +0100

    added memory manager commands

--- trx_v2-firmware/attiny85.ino
+++ trx_v2-firmware/attiny85.ino
@@ -4,13 +4,29 @@
 #include <Wire.h>
 #include <EEPROM.h>
 
-#include "../include/radio_cmds.h"
-
 /*
 ** PB2 (SCL) and PB0 (SDA) are I2C
 ** PB1 is unused
 ** PB3 (vfwd) and PB4 (vref)
 */
+
+// cat commands
+#define CMD_READ_PB1_DIGITAL 1
+#define CMD_READ_PB1_ANALOG 2
+#define CMD_SET_PB1 3
+#define CMD_GET_SERIAL 4
+#define CMD_SET_SERIAL 5
+#define CMD_MEM_SET 6
+#define CMD_MEM_GET 7
+#define CMD_RADIO_RESET 8
+
+// cat responses
+#define CMD_RESP_ACK 1
+#define CMD_RESP_READ_PB1_DIGITAL 2
+#define CMD_RESP_READ_PB1_ANALOG 3
+#define CMD_RESP_GET_SERIAL_ACK 4
+#define CMD_RESP_MEM_FAIL 5
+#define CMD_RESP_WRONG_COMMAND 6
 
 #define CAT_RECEIVE_TIMEOUT 500
 #define SERIAL_EEPROM_OFFSET 0
@@ -33,65 +49,47 @@ void(*reset) (void) = 0;
 
 void run_cmd(byte* cmd)
 {
-    int16_t forward, reflected;
+    int16_t measure, offset;
+
     memset(response, 0, 4);
 
     switch(cmd[4]){
 
-    case CMD_GET_FWD: // Digital Read PB1
+    case CMD_READ_PB1_DIGITAL: // Digital Read PB1
         if (pin_mode != INPUT)
         {
             pin_mode = INPUT;
             pinMode(PB1, pin_mode);
         }
-        response[0] = CMD_RESP_GET_FWD_ACK;
-        forward = digitalRead(PB1);
-        memcpy(response+1, &forward, 2);
-        response_available = true;
+        response[0] = CMD_RESP_READ_PB1_DIGITAL;
+        measure = digitalRead(PB1);
+        memcpy(response+1, &measure, 2);
         break;
 
-    case CMD_GET_REF: // Analog Read PB1
+    case CMD_READ_PB1_ANALOG: // Analog Read PB1
         if (pin_mode != INPUT)
         {
             pin_mode = INPUT;
             pinMode(PB1, pin_mode);
         }
-        response[0] = CMD_RESP_GET_REF_ACK;
-        reflected = analogRead(PB1);
-        memcpy(response+1, &reflected, 2);
-        response_available = true;
+        response[0] = CMD_RESP_READ_PB1_ANALOG;
+        measure = analogRead(PB1);
+        memcpy(response+1, &measure, 2);
         break;
 
-    case CMD_GET_LED_STATUS: // DigitalRead(PB1) output
+    case CMD_SET_PB1: // SET PB1
         if (pin_mode != OUTPUT)
         {
             pin_mode = OUTPUT;
             pinMode(PB1, pin_mode);
-            digitalWrite(PB1, led_status);
-        }
-        if (led_status)
-            response[0] = CMD_RESP_GET_LED_STATUS_ON;
-        else
-            response[0] = CMD_RESP_GET_LED_STATUS_OFF;
-        response_available = true;
-        break;
-
-    case CMD_SET_LED_STATUS: // SET LED STATUS
-        if (pin_mode != OUTPUT)
-        {
-            pin_mode = OUTPUT;
-            pinMode(PB1, pin_mode);
-            digitalWrite(PB1, led_status);
         }
         digitalWrite(PB1, cmd[0]);
         response[0] = CMD_RESP_ACK;
-        response_available = true;
         break;
 
     case CMD_GET_SERIAL: // GET SERIAL NUMBER
         response[0] = CMD_RESP_GET_SERIAL_ACK;
         memcpy(response+1, &serial, 3); // we allocate 24 bit for the serial
-        response_available = true;
       break;
 
     case CMD_SET_SERIAL: // SET SERIAL NUMBER
@@ -101,9 +99,29 @@ void run_cmd(byte* cmd)
         EEPROM.update(SERIAL_EEPROM_OFFSET, serial & 0xff);
         EEPROM.update(SERIAL_EEPROM_OFFSET+1, (serial >> 8) & 0xff) ;
         EEPROM.update(SERIAL_EEPROM_OFFSET+2, (serial >> 16) & 0xff) ;
-        EEPROM.put(SERIAL_EEPROM_OFFSET, serial);
         response[0] = CMD_RESP_ACK;
-        response_available = true;
+        break;
+
+    case CMD_MEM_SET: //
+        memcpy(&offset, cmd, 2);
+        if (offset >= 512)
+            response[0] = CMD_RESP_MEM_FAIL;
+        else
+        {
+            memcpy(&measure, cmd, 2);
+            EEPROM.update(offset, measure & 0xff);
+            response[0] = CMD_RESP_ACK;
+        }
+        break;
+    case CMD_MEM_GET: //
+        memcpy(&offset, cmd, 2);
+        if (offset >= 512)
+            response[0] = CMD_RESP_MEM_FAIL;
+        else
+        {
+            response[0] = CMD_RESP_ACK;
+            response[1] = EEPROM.read(offset);
+        }
         break;
 
     case CMD_RADIO_RESET: // RADIO RESET
@@ -112,8 +130,9 @@ void run_cmd(byte* cmd)
 
     default:
         response[0] = CMD_RESP_WRONG_COMMAND;
-        Wire.write(response, 1);
+        break;
     }
+    response_available = true;
 
 }
 
@@ -166,13 +185,21 @@ void requestEvent()
 {
     int16_t fwd, ref;
 
-    fwd = analogRead(PB3);
-    ref = analogRead(PB4);
-    message[0] = fwd & 0xff;
-    message[1] = fwd >> 8;
-    message[2] = ref & 0xff;
-    message[3] = ref >> 8;
-    Wire.write(message, 4); // 4 bytes message with fwd and ref
+    if (response_available == true)
+    {
+        Wire.write(response, 4); // 4 bytes message with fwd and ref
+        response_available = false;
+    }
+    else
+    {
+        fwd = analogRead(PB3);
+        ref = analogRead(PB4);
+        message[0] = fwd & 0xff;
+        message[1] = fwd >> 8;
+        message[2] = ref & 0xff;
+        message[3] = ref >> 8;
+        Wire.write(message, 4); // 4 bytes message with fwd and ref
+    }
 }
 
 void setup()
@@ -191,7 +218,6 @@ void setup()
     pinMode(PB1, pin_mode);
     pinMode(PB3, INPUT);
     pinMode(PB4, INPUT);
-
 
 }
 
